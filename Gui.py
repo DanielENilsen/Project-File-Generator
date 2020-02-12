a62f561c039114e3c0da58f1767b3dd91721fad8
DanielENilsen <danieleidenilsen@hotmail.com>
2020-02-12 20:54:42 +0100

    update

--- Gui.py
+++ Gui.py
@@ -4,22 +4,21 @@ from tkinter import filedialog
 
 
 def file_dialog(event):
-	root.askdirectory =
-
-
-
-
+	root.filename = filedialog.askopenfilename()
 
 
 
 root = Tk()
 
-
 root.geometry("400x200")
 root.resizable(False,False)
 
-btn = ttk.Button(root, text= "Hello").pack()
+btn = ttk.Button(root, text= "Choose Folder")
+btn.bind("<Button-1>",file_dialog)
+btn.pack()
 
 
+String_var = StringVar()
+Select_projects = ttk.Combobox(root, textvariable = String_var,values=["Standard", "Bootstrap", "SASS"]).pack()
 
 root.mainloop()
