6c72223fe91547f75b760945026c1b03cb2ec1fd
DanielENilsen <danieleidenilsen@hotmail.com>
2020-02-17 16:16:10 +0100

    update

--- Gui.py
+++ Gui.py
@@ -1,28 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkfilebrowser import askopendirname 
+
+
 
 
 class File_Gui(tk.Tk):
-	def __init__(self):
+    
+	def __init__(self,StringVar):
 		tk.Tk.__init__(self)
 		self.title = "File Generator"
 		self.geometry("400x200")
+		self.resizable(False,False)
 		self.button = ttk.Button(self,text="Browse",command=self.searc_button).pack()
-		self.
+		self.StringVar = StringVar
+		self.combo = ttk.Combobox(self,textvariable = self.StringVar,values=["Standard", "Bootstrap", "SASS"]).pack()	
+		self.name_entry = ttk.Entry(self).pack()
+		self.buttonTwo = ttk.Button(self,text="Create Folder", command = self.create_folder).pack()
+
+
+	def create_folder(self):
+		pass
+
+
+		
 
 	def searc_button(self):
-		pass
+
+		
 		
 
 
 
-
-
-
-
-
 if __name__ == "__main__":
-	Gui = File_Gui()
+	Gui = File_Gui("StringVar")
 	Gui.mainloop()
 
