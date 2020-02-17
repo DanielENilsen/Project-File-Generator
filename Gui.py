5f346e97ac3912673fda6cf25f33ed16ebd3048e
DanielENilsen <danieleidenilsen@hotmail.com>
2020-02-17 19:16:32 +0100

    update

--- Gui.py
+++ Gui.py
@@ -1,39 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-from tkfilebrowser import askopendirname 
-
-
+from tkinter import messagebox
+import os 
 
 
 class File_Gui(tk.Tk):
     
-	def __init__(self,StringVar):
+	def __init__(self):
 		tk.Tk.__init__(self)
 		self.title = "File Generator"
 		self.geometry("400x200")
 		self.resizable(False,False)
-		self.button = ttk.Button(self,text="Browse",command=self.searc_button).pack()
-		self.StringVar = StringVar
+		self.StringVar = tk.StringVar()
 		self.combo = ttk.Combobox(self,textvariable = self.StringVar,values=["Standard", "Bootstrap", "SASS"]).pack()	
 		self.name_entry = ttk.Entry(self).pack()
 		self.buttonTwo = ttk.Button(self,text="Create Folder", command = self.create_folder).pack()
 
+		
 
 	def create_folder(self):
-		pass
+		self.filename = filedialog.askdirectory()
+		
 
+		try:
+			self.find = os.chdir(self.filename)
+			self.createFolder = os.mkdir("Project")
+			self.openFolder = os.startfile(f"{self.filename}\\Project")
+		
+		except:
+			self.errormes = messagebox.showerror("Error", "The are an existing folder")
+		
+	
+			
+
+	   
+
+	
+	
 
 		
 
-	def searc_button(self):
+	
 
-		
-		
 
 
 
 if __name__ == "__main__":
-	Gui = File_Gui("StringVar")
+	Gui = File_Gui()
 	Gui.mainloop()
 
