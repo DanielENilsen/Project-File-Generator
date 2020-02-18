7195f019a1405c80f1335b238e9b1e331aff3ea5
DanielENilsen <danieleidenilsen@hotmail.com>
2020-02-18 19:42:51 +0100

    update

--- Gui.py
+++ Gui.py
@@ -13,10 +13,11 @@ class File_Gui(tk.Tk):
 		self.geometry("400x200")
 		self.resizable(False,False)
 		self.StringVar = tk.StringVar()
+		self.intvar = tk.IntVar()
 		self.combo = ttk.Combobox(self,textvariable = self.StringVar,values=["Standard", "Bootstrap", "SASS"]).pack()	
 		self.name_entry = ttk.Entry(self).pack()
 		self.buttonTwo = ttk.Button(self,text="Create Folder", command = self.create_folder).pack()
-		self.checbtnOne = ttk.Checkbutton(self,text="Lagre project").pack()
+		self.checbtnOne = ttk.Checkbutton(self,text="Large projects",variable=self.intvar).pack()
 
 		
 
@@ -28,9 +29,19 @@ class File_Gui(tk.Tk):
 			self.find = os.chdir(self.filename)
 			self.createFolder = os.mkdir("Project")
 			self.openFolder = os.startfile(f"{self.filename}\\Project")
-		
+
+			if self.intvar.get() == 1:
+				self.find = os.chdir(f"{self.filename}\\Project")		
+				self.createFolder = os.mkdir("Script"),os.mkdir("CSS")
+
+	
 		except:
 			self.errormes = messagebox.showerror("Error", "The are an existing folder")
+
+
+	def Larger_projects(self):
+		pass 
+
 		
 	
 
